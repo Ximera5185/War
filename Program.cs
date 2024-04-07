71bfc168622fa1509a29b69fade3bd10a34c8888
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-04-07 14:33:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+        }
+    }
+}
