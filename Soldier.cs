0080f9efdd613ef83cf1c890425295bd269485ad
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-04-08 10:43:25 +0300

    1

--- Soldier.cs
+++ Soldier.cs
@@ -12,12 +12,27 @@ namespace War
         public int Damage { get; private set; }
         public int Armor { get; private set; }
 
-        public bool isDamageMultiplier { get; private set; }    // Множитель урона
-        public bool isMultipleUnitAttack { get; private set; }  // Умение атаковать нескольких солдат
-        public bool isAttackedBy { get; private set;}           // Атакован
-        public bool isRepeatAttack {  get; private set; }       // Умение повторить атаку
+        public bool IsDamageMultiplier { get; private set; }    // Множитель урона
+        public bool IsMultipleUnitAttack { get; private set; }  // Умение атаковать нескольких солдат
+        public bool IsAttackedBy { get; private set;}           // Атакован
+        public bool IsRepeatAttack {  get; private set; }       // Умение повторить атаку
 
-        public Soldier() { }
+        public Soldier(int health,int damage,int armor, bool isDamageMultiplier, bool isMultipleUnitAttack, bool isAttackedBy, bool isRepeatAttack)
+        {
+            Health = health;
+
+            Damage = damage;
+
+            Armor = armor;
+
+            IsDamageMultiplier = isDamageMultiplier;
+
+            IsMultipleUnitAttack = isMultipleUnitAttack;
+
+            IsAttackedBy = isAttackedBy;
+
+            IsRepeatAttack = isRepeatAttack;
+        }
 
         public void TakeDmage(int damage) 
         {
