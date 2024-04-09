01109de31709ebfe792256541e8f47c52f7fcf7d
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-04-09 03:55:31 +0300

    1

--- CreatorSoldier.cs
+++ CreatorSoldier.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace War
 {
-    internal class Platoon
+    internal class CreatorSoldier
     {
-        private List<Soldier> soldiers = new List<Soldier>();
+        
     }
 }
